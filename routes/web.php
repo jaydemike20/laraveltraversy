14c63e47934a103fcb207303141860a98c610675
jaydemike21 <jaydemike21@gmail.com>
2024-10-01 20:11:45 +0800

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// for views and routing (!dont forget the semicolon)
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', function() {
+    return response('<h1>Hello World</h1>', 404)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function($id) {
+    // die and dump- this will
+    dd($id);
+    return response('Post' . $id);
+})->where('id', '[0-9]+');
+
